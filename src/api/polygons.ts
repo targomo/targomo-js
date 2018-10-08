6e566bbe3ceee140d4b3bdc83cbbd9e85fe78bf7
Niek de Wit <niekdewit@live.nl>
2018-10-08 12:44:56 +0200

    remove duplicate code

--- src/api/polygons.ts
+++ src/api/polygons.ts
@@ -14,24 +14,22 @@ export class PolygonsClient {
    * Request geojson polygons for one or more sources from r360 service
    */
   async fetchGeojson(sources: LatLngId[], options: PolygonRequestOptions): Promise<{}> {
-    const url = UrlUtil.buildTargomoUrl(this.client.serviceUrl, 'polygon', this.client.serviceKey)
     const cfg = new PolygonRequestPayload(this.client, sources, options, 'geojson')
-    const result = await requests(this.client, options).fetchCachedData(options.useClientCache, url, 'POST', cfg)
-
-    result.metadata = options
-    return result
+    return await this.fetch(sources, options, cfg);
   }
 
   /**
    * Request svg polygons for one or more sources from r360 service
    */
   async fetchSvg(sources: LatLngId[], options: PolygonRequestOptions): Promise<PolygonSvgResult> {
-    const url = UrlUtil.buildTargomoUrl(this.client.serviceUrl, 'polygon', this.client.serviceKey)
     const cfg = new PolygonRequestPayload(this.client, sources, options, 'json')
+    return await this.fetch(sources, options, cfg) as PolygonSvgResult;
+  }
+
+  private async fetch(sources: LatLngId[], options: PolygonRequestOptions, cfg: PolygonRequestPayload): Promise<{}> {
+    const url = UrlUtil.buildTargomoUrl(this.client.serviceUrl, 'polygon', this.client.serviceKey)
     const result = await requests(this.client, options).fetchCachedData(options.useClientCache, url, 'POST', cfg) as PolygonSvgResult
-
     result.metadata = options
-
     return result
   }
 }
