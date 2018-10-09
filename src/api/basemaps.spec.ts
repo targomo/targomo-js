2aad42460e461974af32f36280475865de464212
Niek de Wit <niekdewit@live.nl>
2018-10-09 12:01:42 +0200

    test failed again oops

--- src/api/basemaps.spec.ts
+++ src/api/basemaps.spec.ts
@@ -5,7 +5,7 @@ describe('TargomoClient basemaps service', () => {
   const testClient = new TargomoClient('germany', process.env.TGM_TEST_API_KEY)
 
   test('get basemap', async () => {
-    expect(testClient.basemaps.getGLStyle('basic'))
+    expect(testClient.basemaps.getGLStyle('Basic'))
     .toEqual('https://maps.targomo.com/styles/klokantech-basic-gl-style.json?key=' + process.env.TGM_TEST_API_KEY);
   })
 })
