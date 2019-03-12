6015fccb251121f61c0f0b0c78e66be610785467
Manuel Tomis <manuel@codegremlins.com>
2019-03-12 11:36:01 +0100

    build for publishing to releases

--- build.js
+++ build.js
@@ -4,13 +4,21 @@ const typescript = require('rollup-plugin-typescript2')
 const copy = require('rollup-plugin-copy')
 const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
+const paths = require('path')
+const fsExtra = require('fs-extra')
 
+const curVersionString = require("./package.json").version
 const curVersion = JSON.stringify(require("./package.json").version)
 const curYear = new Date().getFullYear()
 const author = require("./package.json").author || ''
 const contributors = require("./package.json").contributors || []
 const description = require("./package.json").description || ''
 const name = require("./package.json").name || ''
+
+const which = 'core'
+const distFolder = `./dist/${which}/`
+const targetReleaseFolder = which
+const targetFile = `targomo-${which}`
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -31,75 +39,101 @@ const defaultPlugins = [
   commonjs()
 ]
 
+async function buildLibraries() {
 
-// --- BROWSER ---
+  fsExtra.ensureDirSync(distFolder)
 
-// Regular bundle
-rollup.rollup({
-  input: './src/index.browser.ts',
-  context: 'window',
-  plugins: defaultPlugins,
-}).then(bundle => {
-  bundle.write({
-    name: 'tgm',
-    sourcemap: true,
-    format: 'umd',
-    banner: getBanner(),
-    file: './dist/targomo-core.umd.js'
+
+  // --- BROWSER ---
+
+  // Regular bundle
+  await rollup.rollup({
+    input: './src/index.browser.ts',
+    context: 'window',
+    plugins: defaultPlugins,
+  }).then(bundle => {
+    bundle.write({
+      name: 'tgm',
+      sourcemap: true,
+      format: 'umd',
+      banner: getBanner(),
+      file: paths.join(distFolder, 'targomo-core.umd.js')
+    })
   })
-})
 
-let bannercomment0 = false;
+  let bannercomment0 = false;
 
-// Minified bundle
-rollup.rollup({
-  input: './src/index.browser.ts',
-  context: 'window',
-  plugins: [
-    ...defaultPlugins,
-    uglify({
-      output: {
-        comments: function (node, comment) {
-          var text = comment.value
-          var type = comment.type
-          if (type == "comment2") {
-            // multiline comment
-            const show = !bannercomment0
-            bannercomment0 = true
-            return show
+  // Minified bundle
+  await rollup.rollup({
+    input: './src/index.browser.ts',
+    context: 'window',
+    plugins: [
+      ...defaultPlugins,
+      uglify({
+        output: {
+          comments: function (node, comment) {
+            var text = comment.value
+            var type = comment.type
+            if (type == "comment2") {
+              // multiline comment
+              const show = !bannercomment0
+              bannercomment0 = true
+              return show
+            }
           }
         }
-      }
-    }),
-    copy({
-      "./package.json": "dist/package.json",
-      verbose: true
+      }),
+      copy({
+        "./package.json": paths.join(distFolder, "package.json"),
+        verbose: true
+      })
+    ],
+  }).then(bundle => {
+    bundle.write({
+      name: 'tgm',
+      sourcemap: true,
+      format: 'umd',
+      banner: getBanner(),
+      file: paths.join(distFolder, 'targomo-core.umd.min.js'),
     })
-  ],
-}).then(bundle => {
-  bundle.write({
-    name: 'tgm',
-    sourcemap: true,
-    format: 'umd',
-    banner: getBanner(),
-    file: './dist/targomo-core.umd.min.js',
   })
-})
 
-// --- NODE ---
+  // --- NODE ---
 
-// Regular bundle
-rollup.rollup({
-  input: './src/index.node.ts',
-  external: ['isomorphic-fetch'],
-  plugins: defaultPlugins,
-}).then(bundle => {
-  bundle.write({
-    name: 'tgm',
-    sourcemap: true,
-    format: 'umd',
-    banner: getBanner(),
-    file: './dist/targomo-core.js'
+  // Regular bundle
+  await rollup.rollup({
+    input: './src/index.node.ts',
+    external: ['isomorphic-fetch'],
+    plugins: defaultPlugins,
+  }).then(bundle => {
+    bundle.write({
+      name: 'tgm',
+      sourcemap: true,
+      format: 'umd',
+      banner: getBanner(),
+      file: paths.join(distFolder, 'targomo-core.js'),
+    })
   })
-})
+}
 
+function prepareReleases() {
+  const releaseFolder = paths.resolve(__dirname, 'dist', 'releases', targetReleaseFolder)
+
+  fsExtra.ensureDirSync(releaseFolder)
+
+  // const postfixes = [['', ''], ['', '.min'], ['-full', ''], ['-full', '.min']]
+  const postfixes = [['', ''], ['', '.min']]
+  postfixes.forEach((pair) => {
+    const prefix = pair[0]
+    const postfix = pair[1]
+    fsExtra.copySync(paths.resolve(__dirname, distFolder, targetFile + `${prefix}.umd${postfix}.js`), paths.join(releaseFolder, `latest${prefix}${postfix}.js`))
+    fsExtra.copySync(paths.resolve(__dirname, distFolder, targetFile + `${prefix}.umd${postfix}.js`), paths.join(releaseFolder, `${curVersionString}${prefix}${postfix}.js`))
+  })  
+}
+
+async function buildAll() {
+  await buildLibraries()
+  await prepareReleases()
+}
+
+buildAll()
