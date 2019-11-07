38f1e2776d4ce91292af221d1fc9d8be06c410ae
hfs <github@knackich.de>
2019-11-07 14:32:48 +0100

    Stateful Multigraph: Implement full support  (#60)
    * Bump version number
    
    * POST-RAW requests: Don't parse response
    
    When using the pseudo-method "POST-RAW", in additional to not parsing
    the request body, also don't automatically parse the response body.
    Instead, just return the unmodified response as plain string.
    
    This is needed for requests which return a plain-text response and not
    JSON.
    
    Adapt the POI client which uses POST-RAW to parse as JSON itself.
    
    * Stateful Multigraph: Implement full support
    
    Expand the Multigraph options and the Stateful Multigraph request
    payload with all parameters supported nowadays.
    
    All requests to the Stateful Multigraph must send a serviceUrl URL
    parameter just like the Statistics Service on the production version.
    
    When creating a new Multigraph, request its UUID as plain-text and don't
    try to parse it as JSON.
    
    Provide a tiles URL to be used as tile layer source for maps.

--- src/types/options/multigraphRequestOptions.ts
+++ src/types/options/multigraphRequestOptions.ts
@@ -8,35 +8,62 @@ export enum MultigraphRequestAggregation {
   MEAN = 'mean',
   MEDIAN = 'median',
   NEAREST = 'nearest',
-  UNION = 'routing_union'
+  UNION = 'routing_union',
+  MATH = 'math',
+  GRAVITATION = 'gravitation_huff'
 }
 
 export enum MultigraphRequestLayer {
-  EDGE = 'edge',
-  NODE = 'node',
+  IDENTITY = 'identity',
   TILE = 'tile',
   HEXAGON = 'hexagon',
-  TILE_NODE = 'tile_node',
-  HEXAGON_NODE = 'hexagon_node'
+  CUSTOM_GEOMETRIES = 'custom_geometries'
 }
+
+export interface MultigraphAggregationOptions {
+  type: MultigraphRequestAggregation
+  ignoreOutliers?: boolean
+  outlierPenalty?: number
+  minSourcesRatio?: number
+  minSourcesCount?: number
+  maxResultValue?: number
+  maxResultValueRatio?: number
+  filterValuesForSourceOrigins?: string[]
+  gravitationExponent?: number
+  mathExpression?: string
+  postAggregationFactor?: number
+  aggregationInputParameters?: {
+    [parameterName: string]: {
+      inputFactor?: number
+      gravitationAttractionStrength?: number
+      gravitationPositiveInfluence?: number
+    }
+  }
+}
+
 
 export interface MultigraphSpecificRequestOptions {
 
-  aggregation: {
-    type: MultigraphRequestAggregation
-    ignoreOutliers?: boolean
-    outlierPenalty?: number
-    minSourcesRatio?: number
-    minSourcesCount?: number
-    maxResultValue?: number
-    maxResultValueRatio?: number
-    filterValuesForSourceOrigins?: string[]
+  preAggregationPipeline: {
+    [pipelineName: string]: MultigraphAggregationOptions
   }
+
+  referencedStatisticIds: {
+    [parameterName: string]: number
+  }
+
+  aggregation: MultigraphAggregationOptions
 
   serialization: {
     format: 'geojson' | 'json' | 'mvt'
     decimalPrecision?: number
     maxGeometryCount?: number
+  }
+
+  domain: {
+    type: 'edge' | 'node' | 'statistic_geometry'
+    edgeAggregationType?: 'min' | 'max' | 'mean'
+    statisticGroupId?: number
   }
 
   layer: {
@@ -46,6 +73,7 @@ export interface MultigraphSpecificRequestOptions {
     minGeometryDetailLevel?: number
     maxGeometryDetailLevel?: number
     geometryDetailLevel?: number
+    customGeometryMergeAggregation?: 'max' | 'mean' | 'min' | 'sum'
   }
 
   edgeClasses?: number[]
