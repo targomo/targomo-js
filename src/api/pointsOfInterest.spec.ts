ac3948f095380ef944f68390e533a775d90ddb8e
Max Medina <maxmedina05@gmail.com>
2020-06-22 11:49:43 +0200

    remove failing test (#74)
    * remove failing test
    
    * bump
    
    * remove query from poi service
    
    * update topic definition
    
    * fix lint
    
    Co-authored-by: Max Medina <max.medina@targomo.com>

--- src/api/pointsOfInterest.spec.ts
+++ src/api/pointsOfInterest.spec.ts
@@ -1,7 +1,6 @@
 import { TargomoClient } from './targomoClient';
-import { geometry } from '../index';
 
-describe('TargomoClient poi service', () => {
+describe.only('TargomoClient poi service', () => {
   const testClient = new TargomoClient('centraleurope', process.env.TGM_TEST_API_KEY)
 
   test('poi service request', async () => {
@@ -13,21 +12,5 @@ describe('TargomoClient poi service', () => {
         osmTypes: [{key: 'amenity', value: 'bank'}]
     })
     expect(result).toBeDefined()
-  })
-
-
-  test('poi overpass query', async () => {
-    const sources = { lat: 52.5330232, lng: 13.356626, id: 1}
-    const bounds = geometry.boundingBox(sources, 1)
-
-    const result = await testClient.pois.query( [{type: 'amenity', tag: 'bank'}], [bounds])
-
-    expect(result).toBeDefined()
-    expect(result.length).toBeGreaterThan(0)
-    expect(result[0]).toBeDefined()
-    expect(result[0].id).toBeDefined()
-    expect(result[0].lat).toBeDefined()
-    expect(result[0].lng).toBeDefined()
-    expect(result[0].properties).toBeDefined()
   })
 })
--- src/api/pointsOfInterest.ts
+++ src/api/pointsOfInterest.ts
@@ -1,4 +1,4 @@
-import { BoundingBox, LatLngId, LatLngIdProperties, LatLngProperties } from '../types';
+import { LatLngId, LatLngIdProperties, LatLngProperties } from '../types';
 import { POIRequestOptions } from '../types/options/poiRequestOptions';
 import { requests } from '../util/requestUtil';
 import { POIRequestPayload } from './payload/poiRequestPayload';
@@ -59,60 +59,6 @@ function parseOSMLocation(item: any): OSMLatLng {
 export class PointsOfInterestClient {
   // Idea is this will be instantiated internally in Targomoclient,. and will receive instance of parent in its constructor
   constructor(private client: TargomoClient) {
-  }
-
-
-  // TODO move queryRaw and query to common lib
-
-  /**
-   * Query the overpass service with a raw overpass query string (in other words you supply the exact query)
-   *
-   * @param query
-   */
-  async queryRaw(query: string): Promise<OSMLatLng[]> {
-    let result = await requests(this.client).fetch(this.client.config.overpassUrl + '/api/interpreter', 'POST-RAW', query)
-    result = JSON.parse(result)
-    return result.elements.filter((item: any) => !!item.tags).map((item: any) => parseOSMLocation(item))
-  }
-
-  /**
-   * Query the overpass servive for given categories (way[type]=tag and node[way]=tag) within a given bounding box
-   *
-   * @param categories
-   * @param boundingBox
-   */
-  query(categories: {type: string, tag: string}[], boundingBox: BoundingBox[]): Promise<OSMLatLng[]> {
-    function asQueryElement(query: {type: string, tag: string}) {
-    if (query.tag === '*' || query.tag == null) {
-      return `"${query.type}"`
-    } else if (query.type && query.tag) {
-      return `"${query.type}"="${query.tag}"`
-    } else {
-        throw new Error('invalid query' + JSON.stringify(query))
-      }
-    }
-
-    let params: string[] = []
-    categories.forEach(category => {
-      boundingBox.forEach(box => {
-        params.push(`
-      way[${asQueryElement(category)}]
-        (${box.southWest.lat},${box.southWest.lng},${box.northEast.lat},${box.northEast.lng});
-      node[${asQueryElement(category)}]
-        (${box.southWest.lat},${box.southWest.lng},${box.northEast.lat},${box.northEast.lng});
-        `)
-      })
-    })
-
-    const data = `
-      [out:json];
-      (
-      ${params.join('')}
-      );
-      out center;
-    `
-
-    return this.queryRaw(data)
   }
 
   /**
