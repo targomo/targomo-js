81ff1dea1daac3aea3403cdc42508186b67351ba
Max Medina <maxmedina05@gmail.com>
2020-09-24 10:17:57 +0200

    add env to every requests (#80)
    * add env to every requests
    
    * fix tests
    
    * ignore header if client or config is not available
    
    * add header only if needed
    
    * fix failing tests
    
    * move constants to util folder
    
    * move constants to a global scope
    
    * adjust enum naming to tgm styles
    
    * fix failing tests
    
    Co-authored-by: Max Medina <max.medina@targomo.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './api'
 export * from './types'
 export * from './util'
+export * from './constants'
 
 import * as geometryModule from './geometry'
 export const geometry = geometryModule // NOTE: this does not export types, however we have none in there for now
